44d9f79a6229b64979a89b63a212786d90c637cf
aess <aess@WIN.bug>
2017-11-07 00:58:48 -0200

    Trabalho 2 - Parte 2 semi concluido

--- Trabalho2/js/whatsapp.js
+++ Trabalho2/js/whatsapp.js
@@ -1,165 +1,97 @@
-// let myContacts = [
-//     {
-//         usuario: "joao03",
-//         mensagens: [
-//             { usuario: "joao03", texto: "Tudo bem?" },
-//             { usuario: "victor23", texto: "Tudo Tranqs" },
-//             { usuario: "joao03", texto: "Que bom" }
-//         ]
-//     },
-//     {
-//         usuario: "maria2000",
-//         mensagens: [
-//             { usuario: "maria2000", texto: "Na paz?" },
-//             { usuario: "victor23", texto: "Show" },
-//             { usuario: "maria2000", texto: "Que bom" }
-//         ]
-//     },
-//     {
-//         usuario: "robson_alves",
-//         mensagens: [
-//             { usuario: "victor03", texto: "Bom?" },
-//             { usuario: "robson_alves", texto: "Bom" },
-//             { usuario: "victor03", texto: "Que bom" }
-//         ]
-//     }
-// ];
-
-//Código Ajax apartir daqui
-
-function addNameGroup(groupName) {
-    let listFriends = document.querySelector(".list-chat");
+function addNameGroup(groupName, indice, id) {
+    let listGroups = document.querySelector(".list-grupos");
     let contact = document.createElement("div");
     let spanIconImg = document.createElement("span");
     let spanName = document.createElement("span");
     let textName = document.createTextNode(groupName);
 
     contact.classList.add("contact");
-    spanIconImg.classList.add("icon-user");
-    spanName.classList.add("name-user");
+    spanIconImg.classList.add("icon-group");
+    spanName.classList.add("name-group");
 
     spanName.appendChild(textName);
     contact.appendChild(spanIconImg);
     contact.appendChild(spanName);
 
-    listFriends.appendChild(contact);
+    listGroups.appendChild(contact);
+
+    contact.addEventListener('click', function(){ updateHeadChatGroup(indice)});
+    contact.addEventListener('click', loadPosts(id));
 }
 
-function loadJson(){
+function loadGroups() {
     let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function(){
-        if(this.readyState == 4 && this.status == 200){
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
             let obj_converted = JSON.parse(xhttp.responseText);
-            console.dir(obj_converted);
-            for(let i = 0; i < obj_converted[0].length; i++){
-                console.dir(obj_converted[0][i].groupName);
-                addNameGroup(obj_converted[0][i].groupName);
+            // console.dir(obj_converted);
+            for (let i = 0; i < obj_converted.length; i++) {
+                console.dir(obj_converted[i]);
+                // groupID[i] = obj_converted[i].groupID;
+                addNameGroup(obj_converted[i].groupName, i, obj_converted[i].groupID);
             }
         }
     };
-    xhttp.open('GET', 'http://rest.learncode.academy/api/teste1/groups', 'true');
+    xhttp.open('GET', 'http://rest.learncode.academy/api/teste3/groups', 'true');
+    xhttp.send();
+    // return list;
+}
+
+loadGroups();
+
+let listGroups = document.getElementsByClassName('contact');
+let chatNameGroup = document.querySelector('.chat-name-group');
+
+function updateHeadChatGroup(i){
+    document.getElementById("chat-name-group").innerHTML = "";
+    document.getElementById("chat-icon-group").style.display = "block";
+    document.getElementById("chat-name-group").appendChild(document.createTextNode(listGroups[i].textContent));
+}
+
+function loadPosts(id){
+    document.getElementById("conversations").innerHTML = "";
+    let conversation = document.querySelector(".conversations");
+    let xhttp  = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if (this.readyState == 4 && this.status == 200) {
+            let obj_converted = JSON.parse(xhttp.responseText);
+            console.dir(obj_converted);
+            for(let i = 0; i < obj_converted.length; i++){
+                let post = document.createElement('div').classList.add("posts");
+                let paragrafoUser = document.createElement('p').appendChild(document.createTextNode(obj_converted[i].mensagens[i].userName)); 
+            }
+        }            
+    };
+    let address = 'http://rest.learncode.academy/api/teste4/' + id;
+    xhttp.open('GET', address, true);
     xhttp.send();
 }
 
-loadJson();
-
-// function listContacts() {
-//     for (let i = 0; i < myContacts.length; i++) {
-//         showContacts(myContacts[i].usuario);
-//     }
-//     // for (contact in myContacts){
-//     //     showContacts(myContacts[contact].usuario);
-//     // }
-// }
-
-// listContacts();
-
-// function findPosts(nameUser, postContact) {
-//     let text = "";
-
-//     for (let i = 0; i < myContacts.length; i++) {
-//         let user = myContacts[i].usuario;
-//         if (user == nameUser) {
-//             for (let j = 0; i < myContacts[i].mensagens.length; j++) {
-//                 text = myContacts[i].mensagens[j].texto;
-//                 if (user != myContacts[i].mensagens[j].usuario) {
-//                     postContact.appendChild(function (text) {
-//                         let spanPostSend = document.createElement("span").classList.add("post-send").appendChild(document.createTextNode(text));
-//                         return spanPostSend;
-//                     });
-//                 }
-//                 else {
-//                     postContact.appendChild(function (text) {
-//                         let spanPostReceive = document.createElement("span").classList.add("post-receive").appendChild(document.createTextNode(text));
-//                         return spanPostReceive;
-//                     });
-//                 }
+// function loadPosts(id){
+//     let xhttp = new XMLHttpRequest();
+//     xhttp.onreadystatechange = function(){
+//         if (this.readyState == 4 && this.status == 200) {
+//             let obj_converted = xhttp.responseText;
+//             for (var i = 0; i < obj_converted.length; i++) {
+                
+                
 //             }
-//         }
-//     }
+//         }            
+//     };
+//     let url = 'http://rest.learncode.academy/api/teste3/' + id;
+//     xhttp.open('GET', url, true);
+//     xhttp.send();
 // }
 
-// function createConversation(name) {
-//     let conversation = document.createElement("div");
-//     let head = document.createElement("header");
-//     let spanIconImg = document.createElement("span");
-//     let spanName = document.createElement("span");
-//     let posts = document.createElement("div");
-
-//     let textName = document.createTextNode(name);
-
-//     conversation.classList.add("conversations");
-//     head.classList.add("head-msg");
-//     spanIconImg.classList.add("icon-user");
-//     spanName.classList.add("name-user");
-//     posts.classList.add("posts");
-
-//     spanName.appendChild(textName);
-//     head.appendChild(spanIconImg);
-//     head.appendChild(spanName);
-//     conversation.appendChild(head);
-//     posts = findPosts(textName, posts);
-//     conversation.appendChild(posts);
-
-//     return conversation;
+// for(let i = 0; i < listGroups.length; i++){
+//     listGroups[i].addEventListener('click', function(){
+//         updateHeadChatGroup(listGroups.item(current).children.item(1).textContent);
+//         // setTimeout(function () {
+//         //     if (!listGroups.item(current).classList.contains("active")) {
+//         //         conversation[current].classList.add('active');        
+//         //     }
+//         //             // conversation[current].classList.add('active');        
+//         //     // document.querySelector('.conversations').style.display="block";            
+//         // }, 1000);
+//     });
 // }
-
-// let allConversation = [];
-
-// function loadConversation(){
-//     for(let i = 0; i < myContacts.length; i++){
-//         allConversation.push({usuario: myContacts[i].usuario, conversa: createConversation(myContacts[i].usuario)});
-//     }
-// }
-
-// loadConversation();
-
-// function showPost(user){
-//     let colRight = document.querySelector(".col-right");
-//     let conversation = document.querySelector(".conversations .active");
-
-//     if(conversation.length > 0){
-//         colRight.removeChild(conversation);
-//     }else{
-//         let newConversation;
-//         for(let i=0; i < allConversation.length; i++){
-//             if(user == allConversation[i].usuario){
-//                 newConversation = allConversation[i].conversa;
-//             }
-//         }
-//         newConversation.classList.add("active");
-//         colRight.appendChild(newConversation);
-//     }
-// }
-
-// let startConversation = document.querySelectorAll(".contact");
-
-// function onClick() {
-//     for(let i = 0; i < startConversation[i].length; i++){
-//         startConversation[i].addEventListener('click', function(){
-//             showPost(event.path[0].outerText);
-//         });
-//     }
-// }
-
-// onClick();
